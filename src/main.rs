412287890be5d924fa54ade333cb35c3bda93f44
burgerindividual <burgerindividual@proton.me>
2023-09-25 12:02:54 -0400

    move away from interrupts/rtic, check RTC directly, start work on SIMD

--- src/main.rs
+++ src/main.rs
@@ -1,215 +1,195 @@
 #![feature(variant_count)]
 #![feature(stdsimd)]
+#![feature(portable_simd)]
 #![no_std]
 #![no_main]
 
 mod framebuffer;
 mod pins;
 
-use rtic::app;
+use core::arch::arm::__qadd16;
 // this is used to add the default panic handler, not sure why it goes marked as unused
+use core::arch::arm::dsp::*;
+use core::hint::spin_loop;
+use core::mem::transmute;
+use core::simd::*;
+use core::sync::atomic::{fence, Ordering};
+
+use teensy4_bsp::hal::iomuxc::into_pads;
+use teensy4_bsp::pins::imxrt_iomuxc::gpio::Pin;
+use teensy4_bsp::pins::t40::*;
+use teensy4_bsp::ral::snvs::HPCR;
+use teensy4_bsp::ral::{modify_reg, read_reg, write_reg};
+use teensy4_bsp::{board, ral};
 #[allow(unused_imports)]
 use teensy4_panic as _;
 
-#[app(device = teensy4_bsp, peripherals = true)]
-mod app {
+use crate::framebuffer::*;
+use crate::pins::*;
 
-    use core::hint::spin_loop;
-    use core::sync::atomic::{fence, Ordering};
+/// Probably very bad, gets rid of the memory barriers in exchange for a single yield instruction.
+pub const FAST_MODE: bool = true;
+/// Effectively sets the FPS by masking which bits of the RTC 32khz clock should be tested.
+pub const RTC_MASK: u32 = 0xFFFFFD00;
 
-    use teensy4_bsp::hal::iomuxc::into_pads;
-    use teensy4_bsp::pins::imxrt_iomuxc::gpio::Pin;
-    use teensy4_bsp::pins::t40::*;
-    use teensy4_bsp::ral::gpio::*;
-    use teensy4_bsp::ral::snvs::{HPCR, HPSR, SNVS};
-    use teensy4_bsp::ral::{modify_reg, read_reg, write_reg};
-    use teensy4_bsp::{board, ral};
+#[teensy4_bsp::rt::entry]
+fn main() -> ! {
+    let teensy_peripherals = board::instances();
+    // let cortex_peripherals = Peripherals::take().unwrap();
 
-    use crate::framebuffer::*;
-    use crate::pins::*;
+    // activate GPIO6, GPIO7, and GPIO9 with our used pins
+    write_reg!(
+        ral::gpio,
+        teensy_peripherals.IOMUXC_GPR,
+        GPR26,
+        GPIO6_PIN_MASK
+    );
+    write_reg!(
+        ral::gpio,
+        teensy_peripherals.IOMUXC_GPR,
+        GPR27,
+        GPIO7_PIN_MASK
+    );
+    write_reg!(
+        ral::gpio,
+        teensy_peripherals.IOMUXC_GPR,
+        GPR29,
+        GPIO9_PIN_MASK
+    );
 
-    /// Probably very bad, gets rid of the memory barriers in exchange for a single yield instruction.
-    pub const FAST_MODE: bool = true;
+    let iomuxc = into_pads(teensy_peripherals.IOMUXC);
+    let pins = from_pads(iomuxc);
 
-    #[shared]
-    struct Shared {
-        framebuffer: LedFramebuffer,
+    let mut erased_pins = pins.erase();
+
+    // configure LED output pins
+    for pin in GPIO6_PINS.iter().chain(GPIO7_PINS.iter()) {
+        led_output_pin_setup(&mut erased_pins[pin.pin_index]);
     }
 
-    #[local]
-    struct Local {
-        snvs: SNVS,
-        gpio6: GPIO6,
-        gpio7: GPIO7,
-        gpio9: GPIO9,
+    // configure clock pins
+    clock_pin_setup(&mut erased_pins[2]);
+    clock_pin_setup(&mut erased_pins[3]);
+
+    // set directions for gpio pins
+    modify_reg!(ral::gpio, teensy_peripherals.GPIO6, GDIR, |gdir| gdir
+        | GPIO6_PIN_MASK);
+    modify_reg!(ral::gpio, teensy_peripherals.GPIO7, GDIR, |gdir| gdir
+        | GPIO7_PIN_MASK);
+    modify_reg!(ral::gpio, teensy_peripherals.GPIO9, GDIR, |gdir| gdir
+        | GPIO9_PIN_MASK);
+
+    let mut framebuffer = LedFramebuffer::default();
+
+    // enable RTC and wait for it to get set
+    modify_reg!(ral::gpio, teensy_peripherals.SNVS, HPCR, |hpcr| hpcr
+        | HPCR::RTC_EN::RW::RTC_EN_1);
+    while (read_reg!(ral::gpio, teensy_peripherals.SNVS, HPCR) & HPCR::RTC_EN::mask)
+        != HPCR::RTC_EN::RW::RTC_EN_1
+    {
+        spin_loop();
     }
 
-    #[init]
-    fn init(_: init::Context) -> (Shared, Local) {
-        let teensy_peripherals = board::instances();
-        // let cortex_peripherals = Peripherals::take().unwrap();
+    let mut current_shift_bit = 0_u8;
+    let mut last_rtc_val = 0;
 
-        // activate GPIO6, GPIO7, and GPIO9 with our used pins
-        write_reg!(
-            ral::gpio,
-            teensy_peripherals.IOMUXC_GPR,
-            GPR26,
-            GPIO6_PIN_MASK
+    loop {
+        unsafe {
+            let target_values = framebuffer
+                .bit_target_lines
+                .get_unchecked_mut(current_shift_bit as usize);
+            let current_values = framebuffer
+                .bit_current_lines
+                .get_unchecked_mut(current_shift_bit as usize);
+
+            let mut gpio6_out_buffer = 0_u32;
+            let mut gpio7_out_buffer = 0_u32;
+
+            for pin in GPIO6_PINS {
+                let current_value = current_values.get_unchecked_mut(pin.led_x);
+                let target_value = *target_values.get_unchecked(pin.led_x);
+                let pulse = pwm_pulse(current_value, target_value);
+
+                gpio6_out_buffer |= pulse << pin.offset;
+            }
+
+            for pin in GPIO7_PINS {
+                let current_value = current_values.get_unchecked_mut(pin.led_x);
+                let target_value = *target_values.get_unchecked(pin.led_x);
+                let pulse = pwm_pulse(current_value, target_value);
+
+                gpio7_out_buffer |= pulse << pin.offset;
+            }
+
+            write_reg!(ral::gpio, teensy_peripherals.GPIO6, DR, gpio6_out_buffer);
+            write_reg!(ral::gpio, teensy_peripherals.GPIO7, DR, gpio7_out_buffer);
+
+            let mut clock_pulse = 1 << P2::OFFSET;
+            clock_pulse |= if current_shift_bit == 0 {
+                1 << P3::OFFSET
+            } else {
+                0
+            };
+
+            // 110ns delay?
+            if !FAST_MODE {
+                fence(Ordering::Release);
+            }
+
+            write_reg!(ral::gpio, teensy_peripherals.GPIO9, DR, clock_pulse);
+
+            // 125ns delay?
+            if !FAST_MODE {
+                fence(Ordering::Release);
+            } else {
+                spin_loop();
+            }
+
+            write_reg!(ral::gpio, teensy_peripherals.GPIO9, DR, 0);
+
+            // 10ns delay?
+
+            current_shift_bit += 1;
+            if current_shift_bit == SHIFT_COUNT {
+                current_shift_bit = 0;
+
+                let current_rtc_val =
+                    read_reg!(ral::gpio, teensy_peripherals.SNVS, HPTALR) & RTC_MASK;
+
+                // Frame advance is done here to effectively cause a vertical sync, as we
+                // will only be updating the FB after all scanlines are written.
+                if last_rtc_val != current_rtc_val {
+                    last_rtc_val = current_rtc_val;
+                    frame_advance(&mut framebuffer);
+                }
+            }
+        }
+    }
+}
+
+#[inline(always)]
+pub fn pwm_pulse(current_value: &mut f32, target_value: f32) -> u32 {
+    *current_value += target_value;
+    // this should only ever return 0 or 1, so it should be safe
+    let truncated = unsafe { current_value.to_int_unchecked::<i32>() };
+    *current_value -= truncated as f32;
+    truncated as u32
+}
+
+#[no_mangle]
+pub extern "C" fn pwm_pulse_dsp(current_values: &mut int16x2_t, target_values: int16x2_t) -> u32 {
+    unsafe {
+        let sums = __qadd16(*current_values, target_values);
+        *current_values = transmute::<_, int16x2_t>(
+            transmute::<_, u32>(sums) & 0b0111111111111111_0111111111111111,
         );
-        write_reg!(
-            ral::gpio,
-            teensy_peripherals.IOMUXC_GPR,
-            GPR27,
-            GPIO7_PIN_MASK
-        );
-        write_reg!(
-            ral::gpio,
-            teensy_peripherals.IOMUXC_GPR,
-            GPR29,
-            GPIO9_PIN_MASK
-        );
-
-        let iomuxc = into_pads(teensy_peripherals.IOMUXC);
-        let pins = from_pads(iomuxc);
-
-        let mut erased_pins = pins.erase();
-
-        // configure LED output pins
-        for pin in GPIO6_PINS.iter().chain(GPIO7_PINS.iter()) {
-            led_output_pin_setup(&mut erased_pins[pin.pin_index]);
-        }
-
-        // configure clock pins
-        clock_pin_setup(&mut erased_pins[2]);
-        clock_pin_setup(&mut erased_pins[3]);
-
-        // set directions for gpio pins
-        modify_reg!(ral::gpio, teensy_peripherals.GPIO6, GDIR, |gdir| gdir
-            | GPIO6_PIN_MASK);
-        modify_reg!(ral::gpio, teensy_peripherals.GPIO7, GDIR, |gdir| gdir
-            | GPIO7_PIN_MASK);
-        modify_reg!(ral::gpio, teensy_peripherals.GPIO9, GDIR, |gdir| gdir
-            | GPIO9_PIN_MASK);
-
-        let framebuffer = LedFramebuffer::default();
-
-        // enable RTC and wait for it to get set
-        modify_reg!(ral::gpio, teensy_peripherals.SNVS, HPCR, |hpcr| hpcr
-            | HPCR::RTC_EN::RW::RTC_EN_1);
-        while (read_reg!(ral::gpio, teensy_peripherals.SNVS, HPCR) & HPCR::RTC_EN::mask)
-            != HPCR::RTC_EN::RW::RTC_EN_1
-        {
-            spin_loop();
-        }
-
-        // sets the periodic interrupt frequency to 1/64ths of a second
-        modify_reg!(ral::gpio, teensy_peripherals.SNVS, HPCR, |hpcr| hpcr
-            | (HPCR::PI_FREQ::RW::PI_FREQ_9 << HPCR::PI_FREQ::offset));
-
-        // enable RTC periodic interrupts and wait for it to get set
-        modify_reg!(ral::gpio, teensy_peripherals.SNVS, HPCR, |hpcr| hpcr
-            | (HPCR::PI_EN::RW::PI_EN_1 << HPCR::PI_EN::offset));
-        while ((read_reg!(ral::gpio, teensy_peripherals.SNVS, HPCR) & HPCR::PI_EN::mask)
-            >> HPCR::PI_EN::offset)
-            != HPCR::PI_EN::RW::PI_EN_1
-        {
-            spin_loop();
-        }
-
-        (
-            Shared { framebuffer },
-            Local {
-                snvs: teensy_peripherals.SNVS,
-                gpio6: teensy_peripherals.GPIO6,
-                gpio7: teensy_peripherals.GPIO7,
-                gpio9: teensy_peripherals.GPIO9,
-            },
-        )
+        transmute::<_, u32>(sums) & 0b1000000000000000_1000000000000000
     }
+}
 
-    #[idle(local = [gpio6, gpio7, gpio9], shared = [framebuffer])]
-    fn idle(mut context: idle::Context) -> ! {
-        let mut current_shift_bit = 0_u8;
-
-        loop {
-            context.shared.framebuffer.lock(|framebuffer| unsafe {
-                let target_values = framebuffer
-                    .bit_target_lines
-                    .get_unchecked_mut(current_shift_bit as usize);
-                let current_values = framebuffer
-                    .bit_current_lines
-                    .get_unchecked_mut(current_shift_bit as usize);
-
-                let mut gpio6_out_buffer = 0_u32;
-                let mut gpio7_out_buffer = 0_u32;
-
-                for pin in GPIO6_PINS {
-                    let current_value = current_values.get_unchecked_mut(pin.led_x);
-                    let target_value = *target_values.get_unchecked(pin.led_x);
-                    let pulse = pwm_pulse(current_value, target_value);
-
-                    gpio6_out_buffer |= pulse << pin.offset;
-                }
-
-                for pin in GPIO7_PINS {
-                    let current_value = current_values.get_unchecked_mut(pin.led_x);
-                    let target_value = *target_values.get_unchecked(pin.led_x);
-                    let pulse = pwm_pulse(current_value, target_value);
-
-                    gpio7_out_buffer |= pulse << pin.offset;
-                }
-
-                write_reg!(ral::gpio, context.local.gpio6, DR, gpio6_out_buffer);
-                write_reg!(ral::gpio, context.local.gpio7, DR, gpio7_out_buffer);
-
-                let mut clock_pulse = 1 << P2::OFFSET;
-                clock_pulse |= if current_shift_bit == 0 {
-                    1 << P3::OFFSET
-                } else {
-                    0
-                };
-
-                // 110ns delay?
-                if !FAST_MODE {
-                    fence(Ordering::Release);
-                }
-
-                write_reg!(ral::gpio, context.local.gpio9, DR, clock_pulse);
-
-                // 125ns delay?
-                if !FAST_MODE {
-                    fence(Ordering::Release);
-                } else {
-                    spin_loop();
-                }
-
-                write_reg!(ral::gpio, context.local.gpio9, DR, 0);
-
-                // 10ns delay?
-
-                current_shift_bit += 1;
-                if current_shift_bit == SHIFT_COUNT {
-                    current_shift_bit = 0;
-                }
-            });
-        }
-    }
-
-    #[inline(always)]
-    pub fn pwm_pulse(current_value: &mut f32, target_value: f32) -> u32 {
-        *current_value += target_value;
-        // this should only ever return 0 or 1, so it should be safe
-        let truncated = unsafe { current_value.to_int_unchecked::<i32>() };
-        *current_value -= truncated as f32;
-        truncated as u32
-    }
-
-    #[task(binds = SNVS_HP_WRAPPER, priority = 1, local = [snvs], shared = [framebuffer])]
-    fn frame_advance(mut context: frame_advance::Context) {
-        modify_reg!(ral::gpio, context.local.snvs, HPSR, |hpsr| hpsr
-            | (HPSR::PI::RW::PI_1 << HPSR::PI::offset));
-        context.shared.framebuffer.lock(|framebuffer| unsafe {
-            framebuffer.set_led_unchecked(0, 0, 1.0, 1.0, 1.0);
-        });
+#[inline(always)]
+pub fn frame_advance(framebuffer: &mut LedFramebuffer) {
+    unsafe {
+        framebuffer.set_led_unchecked(0, 0, 1.0, 1.0, 1.0);
     }
 }
